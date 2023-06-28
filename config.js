3c6c81137288b1413363353932e166c87d0a0c61
Sandaru7894 <137981745+Sandaru7894@users.noreply.github.com>
2023-06-29 00:40:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['94763994572', 'sandaru', true],
+  ['94763994572', 'Sandaru_5463', true], 
   [''] 
 ] //Number of owners
 
